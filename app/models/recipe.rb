6ddbabbb189e4a1bbac242a1acfcf34ece591eca
“Jessica <jmychalm13@gmail.com>
2023-10-01 11:04:45 -0500

    added to recipe model to return ingredient list in friendly format

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredients
   has_many :directions
+
+  def ingredients_list
+    list = []
+    ingredients.each do |ingredient|
+      list << ingredient[:name]
+    end
+    return list    
+  end
 end
--- app/views/recipes/_recipe.json.jbuilder
+++ app/views/recipes/_recipe.json.jbuilder
@@ -3,4 +3,5 @@ json.name recipe.name
 json.chef recipe.chef
 json.prep_time recipe.prep_time
 json.cook_time recipe.cook_time
+json.ingredients_list recipe.ingredients_list
 json.temperature recipe.temperature
