80d4f54860e7f21f03632d833f6c9bbf3125bb6d
“Jessica <jmychalm13@gmail.com>
2023-10-13 14:58:11 -0500

    added password_confirmation to match password logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
   has_many :recipes
 end
